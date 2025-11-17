1ca8c51dbf9392f0e7fe8342334ab300d9afbf1f
akshu-mishra <akshita@gkmit.co>
2025-11-17 21:16:23 +0530

    refactor: removed /auth/login as all roles have single login page

--- medilink/src/constants/routes.js
+++ medilink/src/constants/routes.js
@@ -2,19 +2,21 @@ const ROUTES = {
   LOGIN: "/login",
   REGISTER: "/register",
 
-  // Admin Routes
   ADMIN_DASHBOARD: "/admin/dashboard",
   ADMIN_DOCTORS: "/admin/doctors",
 
-  // User Routes
   USER_DASHBOARD: "/user/dashboard",
 
-  // Doctor Routes
   DOCTOR_DASHBOARD: "/doctor/dashboard",
   
-  AUTH_LOGIN: "/auth/login",
   AUTH_REGISTER: "/auth/register",
   AUTH_VALIDATE_TOKEN: "/auth/validate-token",
+
+  CREATE_DOCTOR: "/admin/create",
+
+  UPDATE_DOCTOR: "/admin/update/:id",
+  VIEW_DOCTOR: "/admin/doctor/:id",
+  
 };
 
 export default ROUTES;
